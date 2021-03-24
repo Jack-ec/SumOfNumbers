0e96301f8d1a6a936fa8efbb0266b5a1e87c5d1b
jchid <jchid@192.168.1.79>
2021-03-24 11:31:46 -0600

    started

--- SumOfNumbers/src/SumOfNumbers.java
+++ SumOfNumbers/src/SumOfNumbers.java
@@ -1,4 +1,16 @@
 
 public class SumOfNumbers {
+	
+	public static String getSum(int a, int b) {
+		int startingNumber = a;
+		int endingNumber = b;
+		String lastString = "INVALID";
+		 if (startingNumber > endingNumber) {
+			 return lastString;
+		 }
+		 else {
+			 
+		 }
+	}
 
 }
