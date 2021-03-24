9ede3464b105a5169673f6af362fb99e244345de
jchid <jchid@192.168.1.79>
2021-03-24 13:12:24 -0600

    done

--- SumOfNumbers/src/SumOfNumbers.java
+++ SumOfNumbers/src/SumOfNumbers.java
@@ -1,16 +1,25 @@
 
 public class SumOfNumbers {
-	
+
 	public static String getSum(int a, int b) {
-		int startingNumber = a;
-		int endingNumber = b;
-		String lastString = "INVALID";
-		 if (startingNumber > endingNumber) {
-			 return lastString;
-		 }
-		 else {
-			 
-		 }
+		String equation = "";
+		int sum = 0;
+		int i;
+		if (a <= b) {
+			for (i = a; i <= b; i++) {
+				sum = sum + i;
+				if (i != a) {
+					equation = equation + " + " + i;
+				}
+				else {
+					equation = equation + i;
+				}
+			}
+			return equation + " = " + sum;
+		}
+		else {
+			return "";
+		}
 	}
 
 }
